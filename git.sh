3db5855180513e5ce8346b464ed11b11edf373da
fissart <ricardomallqui6@gmail.com>
2023-01-29 18:59:05 -0500

    w

--- git.sh
+++ git.sh
@@ -1,3 +1,3 @@
 #!/bin/sh/
-git add -A && git commit -m "w" && git commit -m "new" stdbokk.pdf && git push --set-upstream origin master
+git add -A && git commit -a "w" && git push --set-upstream origin master
 #git add --all && git commit -m "Initial comment" && git push -u origin main
