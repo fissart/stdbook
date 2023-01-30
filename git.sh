6ec8622364ea74b0d5b3aab99c319e36acecc83e
fissart <ricardomallqui6@gmail.com>
2023-01-29 19:00:28 -0500

    w

--- git.sh
+++ git.sh
@@ -1,3 +1,3 @@
 #!/bin/sh/
-git add -A && git commit -a "w" && git push --set-upstream origin master
+git rm -r --cached . && git add -A && git commit -am "w" && git push --set-upstream origin master
 #git add --all && git commit -m "Initial comment" && git push -u origin main
