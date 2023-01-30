7c109c3381581c0182855bf1c48c024330fb3f8f
fissart <ricardomallqui6@gmail.com>
2023-01-29 20:34:43 -0500

    w

--- git.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh/
-git rm -r --cached . && git add -A && git commit -am "w" && git push --set-upstream origin master
-#git add --all && git commit -m "Initial comment" && git push -u origin main
